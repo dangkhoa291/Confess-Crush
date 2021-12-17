99f01e00f3a6a8a9b3633d229c5b1069b80a397c
dangkhoa291 <96278358+dangkhoa291@users.noreply.github.com>
2021-12-17 12:21:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    titleWeb: "Demo",
+    introTitle: 'Chào bạn',
+    introDesc: `Ấn vô đi để nhận xét mình qua một năm ( kì ) nào`,
+    btnIntro: 'Nhận xét',
+    title: 'Bạn thấy mình một năm ( kì ) qua thế nào',
+    desc: 'Trả lời thật lòng nha  ',
+    btnYes: 'Bạn đáng iu thật sự ',
+    btnNo: 'Đáng ghét bỏ xừ',
+    question: 'Ủa tại sao vậy ^^',
+    btnReply: 'Nhận xét',
+    reply: 'tại bạn đáng iu quá không từ diễn tả ',
+    mess: 'Mình biết mà 🥰. Cảm ơn bạn nhiều nhiều 😘😘',
+    messDesc: 'Tặng mình con Iphone 13 Pro max nha ',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
